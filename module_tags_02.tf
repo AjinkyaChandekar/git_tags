9ff11afbfe2bf02da92e0bb66a4487739fb74dd9
Ajinkya Chandekar <ajinkya.10705954@ltimindtree.com>
2024-08-17 21:17:13 +0530

    recycle

--- module_tags_02.tf
+++
@@ -1,4 +0,0 @@
-module "tags_02" {
-  source = "git@github.com:AjinkyaChandekar/git_tags.git?ref=v1.0_common_tags"
-
-}
--- outputs_tags_02.tf
+++
@@ -1,3 +0,0 @@
-output "common_tags" {
-  value = module.tags_02.required_tags #refers to outputs.tf from .terraform\modules\tags_02\
-}
